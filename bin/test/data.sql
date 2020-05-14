fdc596327fa96e6b35d7a8bb65cb8c807fb345f1
shony1989@yahoo.com <shony1989@yahoo.com>
2020-05-14 20:44:00 +0200

    unwanted files commited

--- bin/test/data.sql
+++
@@ -1,6 +0,0 @@
-insert into pega_vote_bank_history (country_from, voted_for, year, id) values ('Netherlands', 'Poland', 2020, 1001);
-insert into pega_vote_bank_history (country_from, voted_for, year, id) values ('Netherlands', 'Poland', 2020, 1002);
-insert into pega_vote_bank_history (country_from, voted_for, year, id) values ('Netherlands', 'Poland', 2020, 1003);
-insert into pega_vote_bank_history (country_from, voted_for, year, id) values ('Netherlands', 'Russia', 2020, 1004);
-insert into pega_vote_bank_history (country_from, voted_for, year, id) values ('Netherlands', 'Russia', 2020, 1005);
-insert into pega_vote_bank_history (country_from, voted_for, year, id) values ('Netherlands', 'Belgium', 2020, 1006);
